2e07cfec518148b1df0a7c677ea63de32187a63b
MARZULLO Aldo ICH <amarzullo@ich.techosp.it>
2023-12-14 20:08:47 +0100

    day 9

---
+++ 09/part2.py
@@ -0,0 +1,12 @@
+def solve(line):
+    if sum(line) == 0:
+        return 0
+    return line[0] - solve([line[i+1] - line[i] for i in range(len(line)-1)])
+
+with open('input.txt') as fin:
+    res = 0
+    for line in fin:
+        line = list(map(int, line.strip().split()))
+        res += solve(line)
+
+print(res)
